3be4ce31d10cf2b18ee097beb5863ea91c002988
santyxd3000 <s.pardom@uniandes.edu.co>
2021-03-17 20:38:55 -0500

    Primera Entrega - Reto 2

--- App/controller.py
+++ App/controller.py
@@ -31,8 +31,72 @@ El controlador se encarga de mediar entre la vista y el modelo.
 
 # Inicialización del Catálogo de libros
 
+def initCatalog(typelist):
+    catalog = model.newCatalog(typelist)
+    return catalog
+
+def initcategory():
+    category_ctg = model.newcategory()
+    return category_ctg
+
 # Funciones para la carga de datos
+
+def loadData(catalog):
+    """
+    Carga los datos de los archivos y cargar los datos en la
+    estructura de datos
+    """
+    loadvideo(catalog)
+
+
+def loadvideo(catalog):
+    videofile = cf.data_dir + 'videos/videos-large.csv'
+    input_file = csv.DictReader(open(videofile, encoding='utf-8'))
+    for video in input_file:
+        model.loadData(catalog, video)
+
+
+def loadCategory_id(category_ctg):
+    categoryfile = cf.data_dir + 'videos/category-id.csv'
+    input_file = csv.DictReader(open(categoryfile, encoding="utf-8"),  delimiter='\t')
+    print
+    for category_id in input_file:
+        model.loadCategory_id(category_ctg, category_id)
 
 # Funciones de ordenamiento
 
+def selectionSortVideos(catalog, size):
+
+    return model.selectionSortVideos(catalog, size, 'views')
+
+def shellSortVideos(catalog, size):
+
+    return model.shellSortVideos(catalog, size, 'views')
+
+def insertionSortVideos(catalog, size):
+
+    return model.insertionSortVideos(catalog, size, 'views')
+
+def mergeSortVideos(catalog, size):
+
+    return model.mergeSortVideos(catalog, size, 'views')
+
+def quickSortVideos(catalog, size):
+
+    return model.quickSortVideos(catalog, size, 'views')
+
+
+
 # Funciones de consulta sobre el catálogo
+
+def paisCategoria(catalog, category_ctg, category, country):
+    return model.paisCategoria(catalog, category_ctg, category, country)
+
+def trendingpais(catalog, country):
+    return model.trendingpais(catalog, country)
+
+def trendingcategory(catalog, category_ctg, category):
+    return model.trendingcategory(catalog, category_ctg, category)
+
+def likespaistag(catalog, country, tag):
+    return model.likespaistag(catalog, country, tag)
--- App/view.py
+++ App/view.py
@@ -37,22 +37,166 @@ operación solicitada
 def printMenu():
     print("Bienvenido")
     print("1- Cargar información en el catálogo")
-    print("2- ")
+    print("2- Consultar X videos con más views")
+    print("3: Consultar los X videos mas vistos en un pais para una categoría")
+    print("4- Consultar el video que más días fue trending para un país")
+    print("5- Consultar el video que más días fue trending para una categoría")
+    print("6- Consultar X videos con más likes en un país con un tag específico")
+    print("0- Salir")
 
-catalog = None
+def initCatalog(typelist):
+    return controller.initCatalog(typelist)
+    
+def initcategory():
+    return controller.initcategory()
 
+def loadData(catalog):
+    controller.loadData(catalog)
+
+
+def loadCategory_id(category):
+    controller.loadCategory_id(category)
+
+def printResults(ord_videos, n:int, inputs:int):
+
+    if inputs == 2 and n <= lt.size(ord_videos):
+        print("Los primeros ", n, " videos ordenados son: ")
+        i = 1
+        while i <= n:
+            video = lt.getElement(ord_videos, i)
+            print("Titulo : "+ video['title'] + ' Titulo del Canal: '+ video['channel_title'] +
+            ' Trending_date: ' + video['trending_date']+ ' Pais: '+ video['country'] +
+            ' views: '+ video['views'] + ' Likes: ' + video['likes'] + ' Dislikes: ' + video['dislikes']+ ' Fecha de publicación: '+video['publish_time'])
+            i += 1
+    elif inputs == 3 and n <= lt.size(ord_videos):
+        i = 1
+        while i <= n:
+            video = lt.getElement(ord_videos, i)
+            print(' Trending_date: ' + video['trending_date']+ " Titulo : "+ video['title']+ 
+            ' Titulo del Canal: '+ video['channel_title'] + ' Fecha de publicación: '+video['publish_time']+ 'views: '+ video['views'] + ' Likes: ' +
+             video['likes'] + ' Dislikes: ' + video['dislikes'])
+            i += 1
+    elif inputs == 4 and n <= lt.size(ord_videos):
+        i = 1
+        while i <= n:
+            video = lt.getElement(ord_videos, i)
+            print(" Titulo : "+ video['title']+ ' Titulo del Canal: '+ video['channel_title'] + ' Pais: '+
+            video['country']+ ' Dias de tendencia: ', video['trending_days'])
+            i += 1
+    elif inputs == 5 and n <= lt.size(ord_videos):
+        i = 1
+        while i <= n:
+            video = lt.getElement(ord_videos, i)
+            print(" Titulo : "+ video['title']+ ' Titulo del Canal: '+ video['channel_title'] + ' Categoria_id: '+
+            video['category_id']+ ' Dias de tendencia: ', video['trending_days'])
+            i += 1
+    if inputs == 6 and n <= lt.size(ord_videos):
+        i = 1
+        while i <= n:
+            video = lt.getElement(ord_videos, i)
+            print("Titulo : "+ video['title'] + ' Titulo del Canal: '+ video['channel_title'] + ' Pais: '+ video['country'] +
+            ' Fecha de publicación: '+video['publish_time'] + ' views: '+ video['views'] + ' Likes: ' + video['likes'] + ' Dislikes: ' + 
+            video['dislikes'] + ' Tags: ' + video['tags'])
+            i += 1
 """
 Menu principal
 """
 while True:
     printMenu()
-    inputs = input('Seleccione una opción para continuar\n')
-    if int(inputs[0]) == 1:
+    opcion = input('Seleccione una opción para continuar\n')
+    inputs = int(opcion[0])
+    if inputs == 1:
+        print('Tipo de lista que desea crear')
+        print('1- Array list')
+        print('2- Single linked list')
+        typelist = input('Seleccione el tipo de lista para continuar\n')
         print("Cargando información de los archivos ....")
+        catalog = None
+        if int(typelist[0]) == 1:
+            catalog = initCatalog('ARRAY_LIST')
+            
+        elif int(typelist[0]) == 2:
+            catalog = initCatalog('SINGLE_LIKED')
 
-    elif int(inputs[0]) == 2:
-        pass
+        loadData(catalog)
+        category_ctg = initcategory()
+        loadCategory_id(category_ctg)
+        video = lt.firstElement(catalog)
+        print("Titulo :"+ video['title'] + ' Titulo del Canal: '+ video['channel_title'] +
+            ' Trending_date: ' + video['trending_date']+ ' Pais: '+ video['country'] +
+            ' views: '+ video['views'] + ' Likes: ' + video['likes'] + ' Dislikes: ' + video['dislikes'])
+        print('Videos cargados: ' + str(lt.size(catalog)))
+        print('Catalagos cargados: ' + str(len(category_ctg)))
+        
 
+    elif inputs== 2:
+        size = int(input("Inserte el número de videos: "))
+        print('Metodo de ordenamiento que desea utilizar')
+        print('1- shellsort')
+        print('2- selectionsort')
+        print('3- insertionsort')
+        print('4- mergesort')
+        print('5- quicksort')
+        typesort = input('Seleccione el metodo de ordenamiento para continuar\n')
+        result = None
+        if size <= lt.size(catalog):
+            if int(typesort[0]) == 1:
+                result = controller.shellSortVideos(catalog, (size))
+                print("Para la muestra de", size, " elementos, el tiempo (mseg) es: ",
+                                            str(result[0]))
+                printResults(result[1],10,inputs)
+                
+            elif int(typesort[0]) == 2:
+                result = controller.selectionSortVideos(catalog, size)
+                print("Para la muestra de", size, " elementos, el tiempo (mseg) es: ",
+                                            str(result[0]))
+                printResults(result[1],10,inputs)
+            
+            elif int(typesort[0]) == 3:
+                result = controller.insertionSortVideos(catalog, size)
+                print("Para la muestra de", size, " elementos, el tiempo (mseg) es: ",
+                                            str(result[0]))
+                printResults(result[1],10,inputs)
+            elif int(typesort[0]) == 4:
+                result = controller.mergeSortVideos(catalog, size)
+                print("Para la muestra de", size, " elementos, el tiempo (mseg) es: ",
+                                            str(result[0]))
+                printResults(result[1],10,inputs)
+            elif int(typesort[0]) == 5:
+                result = controller.quickSortVideos(catalog, size)
+                print("Para la muestra de", size, " elementos, el tiempo (mseg) es: ",
+                                            str(result[0]))
+                printResults(result[1],10,inputs)
+
+    elif inputs == 3:
+        pais = str(input("Inserte un país: "))
+        categ = str( input("Inserte una categoría: "))
+        size = int(input("Inserte el número de videos: "))
+        result = controller.paisCategoria(catalog, category_ctg, categ, pais)
+        print("Los primeros ", size, " videos ordenados por views de ",pais, "que pertenecen a la categoría ",
+        categ, " son: ")
+        printResults(result[1],size,inputs)
+
+    
+    elif inputs == 4:
+        pais = str(input("Inserte una pais: "))
+        result = controller.trendingpais(catalog,pais)
+        print("El video que fue trending mas dias de ", pais, " es: ")
+        printResults(result[1],1,inputs)
+
+    elif inputs == 5:
+        categ = str(input("Inserte una categoria: "))
+        result = controller.trendingcategory(catalog, category_ctg, categ)
+        print("El video que fue trending mas dias de la categoría ", categ, " es: ")
+        printResults(result[1],1,inputs)
+
+    elif inputs == 6:
+        size = int(input("Inserte el número de videos: "))
+        pais = str(input("Inserte un país: "))
+        tag = str(input("Inserte un tag específico: "))
+        result = controller.likespaistag(catalog, pais, tag)
+        print("Los primeros ", size, " videos diferentes de ", pais, " con el tag ", tag, " con mas likes son: ")
+        printResults(result[1],size,inputs)
     else:
         sys.exit(0)
 sys.exit(0)
