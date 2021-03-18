be1818611af8d4058c6f8ddd97582afb261a3edb
santyxd3000 <s.pardom@uniandes.edu.co>
2021-03-17 23:46:37 -0500

    Primera Entrega - Reto 2

--- App/model.py
+++ App/model.py
@@ -41,7 +41,7 @@ los mismos.
 # Construccion de modelos
 
 def newCatalog():
-    catalog = {'videos': None,
+    catalog = {'video': None,
                'category_id': None,
                'country': None,
                'trending_days': None,
@@ -49,7 +49,7 @@ def newCatalog():
                'likes': None
                'views': None}
 
-catalog['books'] = lt.newList('SINGLE_LINKED', compareBookIds)
+catalog['video'] = lt.newList('SINGLE_LINKED', cmpVideos)
 catalog['category_id'] = mp.newMap(10000,
                                    maptype='CHAINING',
                                    loadfactor=0.5,
@@ -90,7 +90,85 @@ return catalog
 
 # Funciones para creacion de datos
 
+
+
+
 # Funciones de consulta
+
+def paisCategoria(catalog, category_ctg, category, country):
+        cumple = lt.newList("ARRAY_LIST")
+        for video in catalog["elements"]:
+                if video["category_id"] == category_ctg[category] and video["country"] == country:
+                        loadData(cumple,video)
+        sublist = cumple.copy()
+        return shellSortVideos(sublist,lt.size(sublist),'views')
+ 
+
+def trendingpais(catalog,country):
+        cumple = lt.newList("ARRAY_LIST")
+        trending_days = {}
+        videocopy = None
+        for video in catalog["elements"]:
+                if video["country"] == country:
+                        if video['title'] in trending_days:
+                                pos= int(trending_days[video['title']])
+                                cumple['elements'][pos]['trending_days'] += 1
+                        else:
+                                videocopy = video.copy()
+                                videocopy['trending_days'] = 1
+                                trending_days[video['title']] = lt.size(cumple)
+                                lt.addLast(cumple, videocopy)
+ 
+        return shellSortVideos(cumple,lt.size(cumple),'trending_days')
+
+def trendingcategory(catalog,category_ctg, category):
+        cumple = lt.newList("ARRAY_LIST")
+        trending_days = {}
+        videocopy = None
+        for video in catalog["elements"]:
+                if video["category_id"] == category_ctg[category]:
+                        if video['title'] in trending_days:
+                                pos= int(trending_days[video['title']])
+                                cumple['elements'][pos]['trending_days'] += 1
+                        else:
+                                videocopy = video.copy()
+                                videocopy['trending_days'] = 1
+                                trending_days[video['title']] = lt.size(cumple)
+                                lt.addLast(cumple, videocopy)
+ 
+        return shellSortVideos(cumple,lt.size(cumple),'trending_days')
+                     
+def likespaistag(catalog, country, tag):
+        videos_tags = {}
+        videotags = None
+        cumple = lt.newList("ARRAY_LIST")
+        for video in catalog["elements"]:
+                if video['country'] == country:
+                        videotags = video["tags"]
+                        videotags = videotags.replace('"','')
+                        videotags = videotags.split('|')
+                        ejecutar = True
+                        i = 0
+                        while ejecutar == True and i < len(videotags):
+                                if tag in videotags[i]:
+                                        ejecutar = False
+                                        if video['title'] in videos_tags:
+                                                likes= int(videos_tags[video['title']][1])
+                                                pos = int(videos_tags[video['title']][0])
+                                                if int(video['likes']) > likes:
+                                                        videos_tags[video['title']][1] = video['likes']
+                                                        if lt.size(cumple) == 0:
+                                                                lt.removeFirst(cumple)
+                                                                lt.addFirst(cumple, video)
+                                                        else:
+                                                                lt.deleteElement(cumple, pos)
+                                                                lt.insertElement(cumple, video, pos)
+                                        else:
+                                                videos_tags[video['title']] = [lt.size(cumple),video['likes']]
+                                                lt.addLast(cumple, video)
+                                i += 1
+        return shellSortVideos(cumple,lt.size(cumple),'likes')
+                
 
 # Funciones utilizadas para comparar elementos dentro de una lista
 
@@ -143,3 +221,93 @@ def cmpTags (keyname, tags):
 
 
 # Funciones de ordenamiento
+def selectionSortVideos(catalog, size, parametro):
+        sub_list = lt.subList(catalog, 1, size)
+        sub_list = sub_list.copy()
+        if parametro == 'trending_days':
+                start_time = time.process_time()
+                selectionSortList = sel.sort(sub_list, cmpVideosByTrendingdays)
+                stop_time = time.process_time()
+        elif parametro == 'views':
+                start_time = time.process_time()
+                selectionSortList = sel.sort(sub_list, cmpVideosByViews)
+                stop_time = time.process_time()
+        elif parametro == 'likes':
+                start_time = time.process_time()
+                selectionSortList = sel.sort(sub_list, cmpVideosByLikes)
+                stop_time = time.process_time()
+        elapsed_time_mseg = (stop_time - start_time)*1000
+        return (elapsed_time_mseg, selectionSortList)
+
+
+def shellSortVideos(catalog, size, parametro):
+        sub_list = lt.subList(catalog, 1, size)
+        sub_list = sub_list.copy()
+        if parametro == 'trending_days':
+                start_time = time.process_time()
+                shellSortList = sa.sort(sub_list, cmpVideosByTrendingdays)
+                stop_time = time.process_time()
+        elif parametro == 'views':
+                start_time = time.process_time()
+                shellSortList = sa.sort(sub_list, cmpVideosByViews)
+                stop_time = time.process_time()
+        elif parametro == 'likes':
+                start_time = time.process_time()
+                shellSortList = sa.sort(sub_list, cmpVideosByLikes)
+                stop_time = time.process_time()
+        elapsed_time_mseg = (stop_time - start_time)*1000
+        return (elapsed_time_mseg, shellSortList)
+
+def insertionSortVideos(catalog, size, parametro):
+        sub_list = lt.subList(catalog, 1, size)
+        sub_list = sub_list.copy()
+        if parametro == 'trending_days':
+                start_time = time.process_time()
+                insertionSortList = ins.sort(sub_list, cmpVideosByTrendingdays)
+                stop_time = time.process_time()
+        elif parametro == 'views':
+                start_time = time.process_time()
+                insertionSortList = ins.sort(sub_list, cmpVideosByViews)
+                stop_time = time.process_time()
+        elif parametro == 'likes':
+                start_time = time.process_time()
+                insertionSortList = ins.sort(sub_list, cmpVideosByLikes)
+                stop_time = time.process_time()
+        elapsed_time_mseg = (stop_time - start_time)*1000
+        return (elapsed_time_mseg, insertionSortList)
+        
+def quickSortVideos(catalog, size, parametro):
+        sub_list = lt.subList(catalog, 1, size)
+        sub_list = sub_list.copy()
+        if parametro == 'trending_days':
+                start_time = time.process_time()
+                quickSortList = qui.sort(sub_list, cmpVideosByTrendingdays)
+                stop_time = time.process_time()
+        elif parametro == 'views':
+                start_time = time.process_time()
+                quickSortList = qui.sort(sub_list, cmpVideosByViews)
+                stop_time = time.process_time()
+        elif parametro == 'likes':
+                start_time = time.process_time()
+                quickSortList = qui.sort(sub_list, cmpVideosByLikes)
+                stop_time = time.process_time()
+        elapsed_time_mseg = (stop_time - start_time)*1000
+        return (elapsed_time_mseg, quickSortList)
+
+def mergeSortVideos(catalog, size, parametro):
+        sub_list = lt.subList(catalog, 1, size)
+        sub_list = sub_list.copy()
+        if parametro == 'trending_days':
+                start_time = time.process_time()
+                mergeSortList = mer.sort(sub_list, cmpVideosByTrendingdays)
+                stop_time = time.process_time()
+        elif parametro == 'views':
+                start_time = time.process_time()
+                mergeSortList = mer.sort(sub_list, cmpVideosByViews)
+                stop_time = time.process_time()
+        elif parametro == 'likes':
+                start_time = time.process_time()
+                mergeSortList = mer.sort(sub_list, cmpVideosByLikes)
+                stop_time = time.process_time()
+        elapsed_time_mseg = (stop_time - start_time)*1000
+        return (elapsed_time_mseg, mergeSortList)
